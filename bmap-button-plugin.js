a16bcdfd91307db50cd1450f4637c31532f3a578
lukyanov <y.snaky@gmail.com>
2016-08-08 11:20:13 +0400

    Whitespace formatting

--- bmap-button-plugin.js
+++ bmap-button-plugin.js
@@ -1,114 +1,114 @@
 (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIdentity track validateCode".split(" "), 0);
 function waitFor(obj, prop, func, self,  count) {
-	if (obj[prop]) {
-	  	func.apply(self);
-	} else {
-	  	count = count || 0;
-	  	if (count < 1000) setTimeout(function() {
-	    	waitFor(obj, prop, func, self, count + 1)
-	  	}, 0);
-	}
+    if (obj[prop]) {
+        func.apply(self);
+    } else {
+        count = count || 0;
+        if (count < 1000) setTimeout(function() {
+            waitFor(obj, prop, func, self, count + 1)
+        }, 0);
+    }
 };
 var BMAP = {
-	options: {
-		branchKey:'key_live_blf8cbCOBIE1a7eFuCgqFfhdvDpODoZa',
-		buttonClassName: 'bmap-add-button',
-		lang: (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2) == 'ru' ? 'ru' : 'en',
-	},
-	defaultTitle: {
-		ru: 'Добавить продукты в список',
-		en: 'Add ingredients to shopping list'
-	},
-	sms_text: {
-		ru: 'Добавьте ингредиенты в "Купи батон!"',
-		en: 'Add ingredients to "Buy Mea Pie!"'
-	},
-	mobile: /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent||navigator.vendor||window.opera)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent||navigator.vendor||window.opera).substr(0,4)),
-	extend: function(target, source, overwrite) {
-	  for (var key in source) {
-	    if (overwrite || typeof target[key] === 'undefined') {
-	      target[key] = source[key];
-	    }
-	  }
-	  return target;
-	},
-	init: function(options){
-		this.extend(this.options, options, true);
-		branch.init(this.options.branchKey);
-		waitFor(document, 'body', function() {
-			var node = document.createElement("style");
-			var textnode = document.createTextNode(bmapCSS);
-			node.appendChild(textnode);
-			document.getElementsByTagName('HEAD')[0].appendChild(node);
+    options: {
+        branchKey:'key_live_blf8cbCOBIE1a7eFuCgqFfhdvDpODoZa',
+        buttonClassName: 'bmap-add-button',
+        lang: (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2) == 'ru' ? 'ru' : 'en',
+    },
+    defaultTitle: {
+        ru: 'Добавить продукты в список',
+        en: 'Add ingredients to shopping list'
+    },
+    sms_text: {
+        ru: 'Добавьте ингредиенты в "Купи батон!"',
+        en: 'Add ingredients to "Buy Mea Pie!"'
+    },
+    mobile: /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent||navigator.vendor||window.opera)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent||navigator.vendor||window.opera).substr(0,4)),
+    extend: function(target, source, overwrite) {
+      for (var key in source) {
+        if (overwrite || typeof target[key] === 'undefined') {
+          target[key] = source[key];
+        }
+      }
+      return target;
+    },
+    init: function(options){
+        this.extend(this.options, options, true);
+        branch.init(this.options.branchKey);
+        waitFor(document, 'body', function() {
+            var node = document.createElement("style");
+            var textnode = document.createTextNode(bmapCSS);
+            node.appendChild(textnode);
+            document.getElementsByTagName('HEAD')[0].appendChild(node);
 
-			if (!self.mobile) {
-				var overlayDialog = document.createElement("div");
-				overlayDialog.setAttribute('id','compose-export-button-overlay-dialog');
-				overlayDialog.innerHTML='<div class="compose-export-button-dialog-wrapper"><div class="compose-export-button-dialog"><div id="compose-export-button-close">✕</div><iframe src="" id="compose-export-button-iframe" class="compose-export-button-iframe"></iframe></div></div>';
-				document.getElementsByTagName('BODY')[0].appendChild(overlayDialog);
-				overlayDialog.addEventListener('click',function(){overlayDialog.classList.remove('dialog-shown')});
-				document.getElementById('compose-export-button-close').addEventListener('click',function(event){
-					event.preventDefault();
-					overlayDialog.classList.remove('dialog-shown');
-				});
-			}
-      	});
-		this.apply();
-	},
-	apply: function(){
-	    var buttonsCollection = document.getElementsByClassName(this.options.buttonClassName);
-	    for (var i = buttonsCollection.length - 1; i >= 0; i--) {
-	    	this.makeButton(buttonsCollection[i]);
-	    }
-	},
-	makeButton: function(buttonContainer){
-		var buttonTitle = buttonContainer.getAttribute('button-title')||this.defaultTitle[this.options.lang];
-		var recipe_id = buttonContainer.getAttribute("recipe-id");
-		buttonContainer.innerHTML = '<a href="" id="bmap_'+recipe_id+'" class="compose-export-button" target="_blank">'+buttonTitle+'</a>';
-		this.makeRecipeList(recipe_id)
-	},
-	makeRecipeList: function(recipe_id){
-		var self = this;
-		var button = document.getElementById('bmap_' + recipe_id);
-	    var recipe = document.getElementById(recipe_id);
-	    if (!recipe) return;
-		var items = [];
-		var title, amount, item;
-		var name = recipe.querySelector('[itemprop=name]').innerText;
-		var itemCollection = recipe.querySelectorAll('[itemprop=ingredients]');
-		for (var i = itemCollection.length - 1; i >= 0; i--) {
-			item = itemCollection[i].innerText.split('\n');
-			title = item[0];
-			amount = item[1]||'';
-			items.push({"name":title,"amount":amount,"group":0});
-		}
+            if (!self.mobile) {
+                var overlayDialog = document.createElement("div");
+                overlayDialog.setAttribute('id','compose-export-button-overlay-dialog');
+                overlayDialog.innerHTML='<div class="compose-export-button-dialog-wrapper"><div class="compose-export-button-dialog"><div id="compose-export-button-close">✕</div><iframe src="" id="compose-export-button-iframe" class="compose-export-button-iframe"></iframe></div></div>';
+                document.getElementsByTagName('BODY')[0].appendChild(overlayDialog);
+                overlayDialog.addEventListener('click',function(){overlayDialog.classList.remove('dialog-shown')});
+                document.getElementById('compose-export-button-close').addEventListener('click',function(event){
+                    event.preventDefault();
+                    overlayDialog.classList.remove('dialog-shown');
+                });
+            }
+          });
+        this.apply();
+    },
+    apply: function(){
+        var buttonsCollection = document.getElementsByClassName(this.options.buttonClassName);
+        for (var i = buttonsCollection.length - 1; i >= 0; i--) {
+            this.makeButton(buttonsCollection[i]);
+        }
+    },
+    makeButton: function(buttonContainer){
+        var buttonTitle = buttonContainer.getAttribute('button-title')||this.defaultTitle[this.options.lang];
+        var recipe_id = buttonContainer.getAttribute("recipe-id");
+        buttonContainer.innerHTML = '<a href="" id="bmap_'+recipe_id+'" class="compose-export-button" target="_blank">'+buttonTitle+'</a>';
+        this.makeRecipeList(recipe_id)
+    },
+    makeRecipeList: function(recipe_id){
+        var self = this;
+        var button = document.getElementById('bmap_' + recipe_id);
+        var recipe = document.getElementById(recipe_id);
+        if (!recipe) return;
+        var items = [];
+        var title, amount, item;
+        var name = recipe.querySelector('[itemprop=name]').innerText;
+        var itemCollection = recipe.querySelectorAll('[itemprop=ingredients]');
+        for (var i = itemCollection.length - 1; i >= 0; i--) {
+            item = itemCollection[i].innerText.split('\n');
+            title = item[0];
+            amount = item[1]||'';
+            items.push({"name":title,"amount":amount,"group":0});
+        }
 
-		var export_data = {};
-		export_data['lists'] = [{"name":name,"active":true,"items":items}];
-		export_data['$desktop_deepview'] = 'import_ingredients_' + this.options.lang;
-		export_data['$custom_sms_text'] =  this.sms_text[this.options.lang] + ' {{ link }}';
+        var export_data = {};
+        export_data['lists'] = [{"name":name,"active":true,"items":items}];
+        export_data['$desktop_deepview'] = 'import_ingredients_' + this.options.lang;
+        export_data['$custom_sms_text'] =  this.sms_text[this.options.lang] + ' {{ link }}';
 
-		if (!self.mobile) {
-			var overlayDialog = document.getElementById('compose-export-button-overlay-dialog');
-			button.addEventListener('click',function(event){
-				event.preventDefault();
-				var link = button.getAttribute('href');
-				document.getElementById('compose-export-button-iframe').setAttribute('src',link);
-				overlayDialog.classList.add('dialog-shown');
-			});
-		}
+        if (!self.mobile) {
+            var overlayDialog = document.getElementById('compose-export-button-overlay-dialog');
+            button.addEventListener('click',function(event){
+                event.preventDefault();
+                var link = button.getAttribute('href');
+                document.getElementById('compose-export-button-iframe').setAttribute('src',link);
+                overlayDialog.classList.add('dialog-shown');
+            });
+        }
 
-		branch.link({
-			campaign: 'Partners',
-			channel: 'partner site',
-			tags: ['import', document.location.host],
-			feature: 'import',
-			stage: '',
-			data: export_data
-		}, function(err, link) {
-			button.setAttribute('href',link);
-		})
-	}
+        branch.link({
+            campaign: 'Partners',
+            channel: 'partner site',
+            tags: ['import', document.location.host],
+            feature: 'import',
+            stage: '',
+            data: export_data
+        }, function(err, link) {
+            button.setAttribute('href',link);
+        })
+    }
 };
 
 var bmapCSS = '\
