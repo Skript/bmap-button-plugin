aa5173d2ddd9a9899d21a619cf34d7c5065e1a57
lukyanov <y.snaky@gmail.com>
2016-08-08 12:42:52 +0400

    Wrap all functions and variables into BMAP namespace

--- bmap-button-plugin.js
+++ bmap-button-plugin.js
@@ -1,14 +1,5 @@
 (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIdentity track validateCode".split(" "), 0);
-function waitFor(obj, prop, func, self,  count) {
-    if (obj[prop]) {
-        func.apply(self);
-    } else {
-        count = count || 0;
-        if (count < 1000) setTimeout(function() {
-            waitFor(obj, prop, func, self, count + 1)
-        }, 0);
-    }
-};
+
 var BMAP = {
     options: {
         branchKey:'key_live_blf8cbCOBIE1a7eFuCgqFfhdvDpODoZa',
@@ -23,6 +14,95 @@ var BMAP = {
         ru: 'Добавьте ингредиенты в "Купи батон!"',
         en: 'Add ingredients to "Buy Mea Pie!"'
     },
+    css: '\
+        .compose-export-button,\
+        .compose-export-button:visited,\
+        .compose-export-button:active,\
+        .compose-export-button:focus {\
+            color: #fff !important;\
+            margin: 20px auto;\
+            position: relative;\
+            cursor: pointer;\
+            text-align: center;\
+            vertical-align: middle;\
+            display: inline-block;\
+            text-decoration: none !important;\
+            line-height: 40px;\
+            background: url(https://buymeapie.com/images/logo.png) no-repeat 10px 50% #0095ff;\
+            background-size: 32px;\
+            padding: 0 20px 0 60px;\
+            box-shadow: none;\
+            border-radius: 3px;\
+        }\
+        .compose-export-button:hover {\
+            color: #fff !important;\
+            text-decoration: none;\
+            background-color: #40b0ff;\
+        }\
+#compose-export-button-overlay-dialog {\
+            background-color: rgba(255, 255, 255, 0.75);\
+            display: none;\
+            height: 100%;\
+            left: 0;\
+            position: fixed;\
+            top: 0;\
+            width: 100%;\
+            z-index: 10000;\
+        }\
+#compose-export-button-overlay-dialog.dialog-shown { display: table; }\
+        .compose-export-button-dialog {\
+            display: inline-block;\
+            height: 468px;\
+            width: 840px;\
+            background-color: rgb(255, 255, 255);\
+            border-radius: 5px;\
+            box-sizing: border-box;\
+            display: inline-block;\
+            overflow: auto;\
+            padding: 20px;\
+            position: relative;\
+            text-align: left;\
+            z-index: 100000;\
+            box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);\
+        }\
+        .compose-export-button-dialog-wrapper  {\
+            display: table-cell;\
+            text-align: center !important;\
+            vertical-align: middle !important;\
+        }\
+        .compose-export-button-dialog .compose-export-button-iframe {\
+            border: medium none;\
+            box-sizing: border-box;\
+            height: 100%;\
+            left: 0;\
+            margin: 0;\
+            padding: 0 20px;\
+            position: absolute;\
+            top: 0;\
+            width: 100%;\
+            max-width: inherit !important;\
+        }\
+        .compose-export-button-dialog #compose-export-button-close {\
+            cursor: pointer;\
+            position: absolute;\
+            right: 10px;\
+            top: 10px;\
+            z-index: 1000;\
+            background-position: center center;\
+            background-repeat: no-repeat;\
+            border-radius: 10px;\
+            cursor: pointer;\
+            height: 40px;\
+            width: 40px;\
+            color: #555;\
+            font-size: 20px;\
+            line-height: 40px;\
+            text-align: center;\
+        }\
+        .compose-export-button-dialog #compose-export-button-close:hover {\
+            background-color: #eee;\
+        }\
+    ',
     mobile: /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent||navigator.vendor||window.opera)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent||navigator.vendor||window.opera).substr(0,4)),
     extend: function(target, source, overwrite) {
       for (var key in source) {
@@ -32,12 +112,23 @@ var BMAP = {
       }
       return target;
     },
+    waitFor: function(obj, prop, func, count) {
+        if (obj[prop]) {
+            func.apply();
+        } else {
+            count = count || 0;
+            if (count < 1000) setTimeout(function() {
+                this.waitFor(obj, prop, func, count + 1)
+            }, 0);
+        }
+    },
     init: function(options){
         this.extend(this.options, options, true);
         branch.init(this.options.branchKey);
-        waitFor(document, 'body', function() {
+        var self = this;
+        this.waitFor(document, 'body', function() {
             var node = document.createElement("style");
-            var textnode = document.createTextNode(bmapCSS);
+            var textnode = document.createTextNode(self.css);
             node.appendChild(textnode);
             document.getElementsByTagName('HEAD')[0].appendChild(node);
 
@@ -68,7 +159,6 @@ var BMAP = {
         this.makeRecipeList(buttonContainer, recipe_id)
     },
     makeRecipeList: function(button, recipe_id){
-        var self = this;
         var recipe;
         if (recipe_id) {
             recipe = document.getElementById(recipe_id);
@@ -111,98 +201,9 @@ var BMAP = {
             stage: '',
             data: export_data
         }, function(err, link) {
-            button.setAttribute('href',link);
+            a.setAttribute('href',link);
         })
     }
 };
-
-var bmapCSS = '\
-.compose-export-button,\
-.compose-export-button:visited,\
-.compose-export-button:active,\
-.compose-export-button:focus {\
-    color: #fff !important;\
-    margin: 20px auto;\
-    position: relative;\
-    cursor: pointer;\
-    text-align: center;\
-    vertical-align: middle;\
-    display: inline-block;\
-    text-decoration: none !important;\
-    line-height: 40px;\
-    background: url(https://buymeapie.com/images/logo.png) no-repeat 10px 50% #0095ff;\
-    background-size: 32px;\
-    padding: 0 20px 0 60px;\
-    box-shadow: none;\
-    border-radius: 3px;\
-}\
-.compose-export-button:hover {\
-    color: #fff !important;\
-    text-decoration: none;\
-    background-color: #40b0ff;\
-}\
-#compose-export-button-overlay-dialog {\
-    background-color: rgba(255, 255, 255, 0.75);\
-    display: none;\
-    height: 100%;\
-    left: 0;\
-    position: fixed;\
-    top: 0;\
-    width: 100%;\
-    z-index: 10000;\
-}\
-#compose-export-button-overlay-dialog.dialog-shown { display: table; }\
-.compose-export-button-dialog {\
-    display: inline-block;\
-    height: 468px;\
-    width: 840px;\
-    background-color: rgb(255, 255, 255);\
-    border-radius: 5px;\
-    box-sizing: border-box;\
-    display: inline-block;\
-    overflow: auto;\
-    padding: 20px;\
-    position: relative;\
-    text-align: left;\
-    z-index: 100000;\
-    box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);\
-}\
-.compose-export-button-dialog-wrapper  {\
-    display: table-cell;\
-    text-align: center !important;\
-    vertical-align: middle !important;\
-}\
-.compose-export-button-dialog .compose-export-button-iframe {\
-    border: medium none;\
-    box-sizing: border-box;\
-    height: 100%;\
-    left: 0;\
-    margin: 0;\
-    padding: 0 20px;\
-    position: absolute;\
-    top: 0;\
-    width: 100%;\
-    max-width: inherit !important;\
-}\
-.compose-export-button-dialog #compose-export-button-close {\
-    cursor: pointer;\
-    position: absolute;\
-    right: 10px;\
-    top: 10px;\
-    z-index: 1000;\
-    background-position: center center;\
-    background-repeat: no-repeat;\
-    border-radius: 10px;\
-    cursor: pointer;\
-    height: 40px;\
-    width: 40px;\
-    color: #555;\
-    font-size: 20px;\
-    line-height: 40px;\
-    text-align: center;\
-}\
-.compose-export-button-dialog #compose-export-button-close:hover {\
-  background-color: #eee;\
-}';
 
 window.bmapAsyncInit && setTimeout(bmapAsyncInit, 0);
